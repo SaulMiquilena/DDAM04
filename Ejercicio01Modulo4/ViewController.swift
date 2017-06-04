61e87ff7a8e0de56980aef593bcd49d13e781263
laboratorio <laboratorio@laboratorios-Mac.local>
2017-06-04 07:53:48 -0430

    Initial Commit

---
+++ Ejercicio01Modulo4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ejercicio01Modulo4
+//
+//  Created by laboratorio on 6/4/17.
+//  Copyright (c) 2017 ucla.dcty.fomento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
